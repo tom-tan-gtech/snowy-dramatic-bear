b9791f5bd77d9bf80196c70beeb0a3b4e79cccf2
Glitch (glitch-hello-react) <none>
2024-09-26 07:28:13 +0000

    🎐🏨 Checkpoint ./src/components/router.jsx:34784670/48 ./src/app.jsx:34784670/1146 ./src/styles/styles.css:34784670/3703 ./src/pages/home.jsx:34784670/674 ./package.json:34784670/141 ./src/index.jsx:34784670/106

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,3 @@
-import React, { useState, useEffect } from "react";
-import { Router, Link } from "wouter";
-
 /**
 * This code defines the react app
 *
@@ -14,39 +11,11 @@ import { Router, Link } from "wouter";
 // Import and apply CSS stylesheet
 import "./styles/styles.css";
 
-// Where all of our pages come from
-import PageRouter from "./components/router.jsx";
-
-// The component that adds our Meta tags to the page
-import Seo from './components/seo.jsx';
-
 // Home function that is reflected across the site
 export default function Home() {
   return (
-    <Router>
-      <Seo />
-      <main role="main" className="wrapper">
-        <div className="content">
-          {/* Router specifies which component to insert here as the main content */}
-          <PageRouter />
-        </div>
-      </main>
-      {/* Footer links to Home and About, Link elements matched in router.jsx */}
-      <footer className="footer">
-        <div className="links">
-          <Link href="/">Home</Link>
-          <span className="divider">|</span>
-          <Link href="/about">About</Link>
-        </div>
-        <a
-          className="btn--remix"
-          target="_top"
-          href="https://glitch.com/edit/#!/remix/glitch-hello-react"
-        >
-          <img src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="" />
-          Remix on Glitch
-        </a>
-      </footer>
-    </Router>
+    <main role="main" className="wrapper" id="root">
+      <h1>Dot game</h1>
+    </main>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app.jsx";
-import { HelmetProvider } from 'react-helmet-async';
 
 /**
 * Root of react site 
@@ -13,9 +12,7 @@ import { HelmetProvider } from 'react-helmet-async';
 // Render the site https://reactjs.org/docs/react-dom.html#render
 ReactDOM.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
